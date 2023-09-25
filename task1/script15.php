38054dbdce562793edcc36f1cbead9cf6a97a40d
riyamashru <143238107+riyamashru@users.noreply.github.com>
2023-09-25 10:08:03 +0530

    Delete task1/script15.php

--- task1/script15.php
+++
@@ -1,6 +0,0 @@
-<?php
-$value1 = (integer)readline("Enter value1: ");
-$value2 = (integer)readline("Enter value2: ");
-$value3 = (integer)readline("Enter value3: ");
-echo $value1,$value2,$value3;
-?>
